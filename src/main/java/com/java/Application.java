c2d06575280e2e6807e9962238ca2e9bba831b57
SrinuJava-Code <srinivasarithoti94@gmail.com>
2021-05-31 23:14:06 +0530

    HIS-300 Related  changes

--- src/main/java/com/java/Application.java
+++ src/main/java/com/java/Application.java
@@ -7,8 +7,11 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class Application {
 
+private static final String RESt_URL="https://www.equifax.com/grtScores";
+
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
-	}
+	int a=10; 
+}
 
 }
