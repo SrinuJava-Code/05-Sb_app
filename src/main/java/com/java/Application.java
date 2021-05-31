8ef4fd7b0f18f816023ce0c1c76e7d74102e516a
SrinuJava-Code <srinivasarithoti94@gmail.com>
2021-05-31 23:31:10 +0530

    HIS related changes

--- src/main/java/com/java/Application.java
+++ src/main/java/com/java/Application.java
@@ -7,8 +7,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class Application {
 
+private Map<String,Object> cahe = new HashMap<String,Object>();
+
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
 	}
 
+      public void loadDataToCache(){
+logic //
+}     
 }
